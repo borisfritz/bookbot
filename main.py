ded7414e1e93dd9799c2db4d2f4e8d43cf09a38f
Matthew Grimes <matt.grimz@gmail.com>
2025-08-16 17:59:27 -0600

    A7: added count_letters() to stats.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # Take in file_path as STRING and return file_contents as a STRING
     #if argument IS NOT a STRING return NONE and Print EnvironmentError
-        # TODO: Add error handling
 
 def get_book_txt(file_path):
     if type(file_path) is str:
@@ -11,13 +10,23 @@ def get_book_txt(file_path):
         print("incorrect file path!")
         return None
 
+# MAIN PROGRAM
+
 def main():
 
-    from stats import num_words_in_book
+    from stats import count_words
+    from stats import count_letters
 
+    # Import Book from file and store as a STRING
     book_file_path =  "books/frankenstein.txt"
     book_text = get_book_txt(book_file_path)
-    book_word_count = num_words_in_book(book_text)
+
+    # Find and print the book's word count
+    book_word_count = count_words(book_text)
     print(f"{book_word_count} words found in the document")
+
+    # print 'letter: count' for each letter
+    book_letter_count = count_letters(book_text)
+    print(book_letter_count)
 
 main()
--- stats.py
+++ stats.py
@@ -1,12 +1,29 @@
 # Take in STRING text and return INT word_count
-    # if argument text IS NOT a STRING return NONE
-        # TODO: add error handling
+    # if argument text IS NOT a STRING return NONE and print an error
 
-def num_words_in_book(text):
+def count_words(text):
     if type(text) is str:  
         words = text.split()
         word_count = len(words)
         return word_count
     else:
-        print("num_words() incorrect type")
+        print("count_words() incorrect type")
         return None
+
+# Take in STRING text and return a dictonary of letter -> ammount found in text
+    #if argument text IS NOT a STRING, return none and print error
+
+def count_letters(text):
+    letter_count = {}
+    if type(text) == str:
+        lower_case_text = text.lower()
+        for l in lower_case_text:
+            if l in letter_count:
+                letter_count[l] += 1
+            else:
+                letter_count[l] = 1
+        return letter_count
+    else:
+        print("count_letters() incorrect type")
+        return None
+
