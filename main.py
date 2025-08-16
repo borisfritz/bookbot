2bb0c3cc8ea83e52ba030d792fc93ec197371a3f
Matthew Grimes <matt.grimz@gmail.com>
2025-08-16 16:32:11 -0600

    A4: added get_book_text() to main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text(file_path):
+    if type(file_path) is str:
+        with open(file_path) as f:
+            file_contents = f.read()
+            return file_contents
+    else:
+        print("incorrect file path!")
+        return None
+
+def main():
+    book_file_path =  "books/frankenstein.txt"
+    book_text = get_book_text(book_file_path)
+    print(book_text)
+
+main()
